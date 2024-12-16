904f5ca911204dd550840f81e3a9327af5a57dc3
alexnerd <alexnerd85@gmail.com>
2024-12-16 22:19:05 +0300

    Add title normalizer

---
+++ src/main/java/com/alexnerd/blog/service/adapters/TitleNormalizer.java
@@ -0,0 +1,36 @@
+package com.alexnerd.blog.service.adapters;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TitleNormalizer {
+
+    @Value("nerdblog.title.separator")
+    private String titleSeparator;
+
+    private int codePointSeparator;
+
+    @PostConstruct
+    public void init() {
+        this.codePointSeparator = this.titleSeparator.codePoints()
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public String normalize(String title) {
+        return title.codePoints()
+                .map(this::replaceDigitOrLetter)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    private int replaceDigitOrLetter(int codePoint) {
+        if (Character.isLetterOrDigit(codePoint)) {
+            return codePoint;
+        } else {
+            return codePointSeparator;
+        }
+    }
+}
