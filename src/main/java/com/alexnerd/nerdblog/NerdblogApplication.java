070e59141a8407b205c68a46b578d0031347fc85
alexnerd <alexnerd85@gmail.com>
2024-12-16 20:52:20 +0300

    init commit Spring Boot 3.4.0

---
+++ src/main/java/com/alexnerd/nerdblog/NerdblogApplication.java
@@ -0,0 +1,13 @@
+package com.alexnerd.nerdblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexnerd/nerdblog/NerdblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexnerd.nerdblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
