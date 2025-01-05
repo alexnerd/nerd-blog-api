f16442e62ecd01f60bf3b9fb9f7a7a71f67aec0a
alexnerd <alexnerd85@gmail.com>
2025-01-05 14:59:59 +0300

    add cors support

--- src/main/java/com/alexnerd/blog/system/WebConfig.java
+++ src/main/java/com/alexnerd/blog/system/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET").allowedOrigins("http://javanerd.ru");
+                .allowedMethods("*").allowedOrigins("http://javanerd.ru");
     }
 }
